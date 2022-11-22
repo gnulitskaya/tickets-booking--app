009d9bfbe432705ed5c9b6e52d8d4615c953a37e
gnulitskaya <v.gnulitskaya@finflowsoft.com>
2022-11-22 18:58:59 +0300

    add airplane part

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,4 +1,5 @@
 import 'package:booktickets/utils/app_styles.dart';
+import 'package:booktickets/widgets/thick_container.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -19,9 +20,9 @@ class TicketView extends StatelessWidget {
         margin: const EdgeInsets.only(left: 16),
         child: Column(children: [
           Container(
-            decoration: BoxDecoration(
-              color: const Color(0xFF526799),
-              borderRadius: const BorderRadius.only(
+            decoration: const BoxDecoration(
+              color: Color(0xFF526799),
+              borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(21), topRight: Radius.circular(21)),
             ),
             padding: const EdgeInsets.all(16),
@@ -34,6 +35,43 @@ class TicketView extends StatelessWidget {
                       style:
                           Styles.headLineStyle3.copyWith(color: Colors.white),
                     ),
+                    const Spacer(),
+                    const ThickContainer(),
+                    Expanded(
+                        child: Stack(children: [
+                      SizedBox(
+                        height: 24,
+                        child: LayoutBuilder(
+                          builder: (BuildContext context,
+                              BoxConstraints constraints) {
+                            return Flex(
+                              direction: Axis.horizontal,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              mainAxisSize: MainAxisSize.max,
+                              children: List.generate(
+                                  (constraints.constrainWidth() / 6).floor(),
+                                  (index) => SizedBox(
+                                        width: 3,
+                                        height: 1,
+                                        child: const DecoratedBox(
+                                            decoration: BoxDecoration(
+                                                color: Colors.white)),
+                                      )),
+                            );
+                          },
+                        ),
+                      ),
+                      Center(
+                        child: Transform.rotate(
+                          angle: 1.5,
+                          child: Icon(
+                            Icons.local_airport_rounded,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ])),
+                    const ThickContainer(),
                     const Spacer(),
                     Text(
                       'LDN',
