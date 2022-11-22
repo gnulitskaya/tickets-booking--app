09022d6618eb80a5915d8f3c1c9b997e05f28481
gnulitskaya <v.gnulitskaya@finflowsoft.com>
2022-11-22 17:29:07 +0300

    add layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:booktickets/screens/ticket_view.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -44,10 +46,51 @@ class HomeScreen extends StatelessWidget {
                               image: AssetImage('assets/images/img_1.png'))),
                     )
                   ],
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xFFF4F6Fd)),
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        FluentSystemIcons.ic_fluent_search_regular,
+                        color: Color.fromARGB(255, 189, 188, 194),
+                      ),
+                      Text(
+                        "Search",
+                        style: Styles.headLineStyle4,
+                      )
+                    ],
+                  ),
+                ),
+                const Gap(45),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Upcoming Flights',
+                      style: Styles.headLineStyle2,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        print('dscc');
+                      },
+                      child: Text(
+                        'View all',
+                        style: Styles.textStyle
+                            .copyWith(color: Styles.primaryColor),
+                      ),
+                    ),
+                  ],
                 )
               ],
             ),
-          )
+          ),
+          const Gap(15),
+          TicketView(),
         ],
       ),
     );
